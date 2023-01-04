0ad482e3b66ab4cff69a38d955a2b8fbe44b6a4e
hassana123 <hassanaabdll1@gmail.com>
2023-01-04 15:32:34 +0100

    day one of 30daysOfJs

---
+++ index.js
@@ -0,0 +1,33 @@
+// day ne exercises
+// comments can make code readable
+//Welcome to 30DaysOfJavaScript
+/*comments can make code readable,
+easy to reuse and informative
+*/
+let fname="Asabeneh";
+let age=30;
+let isSingle = false;
+let country;
+let gender=null;
+
+console.log(typeof fname);
+console.log(typeof age);
+console.log(typeof isSingle);
+console.log(typeof gender);
+console.log(typeof country);
+
+let abu;
+let aaaas;
+let mint;
+let toy;
+
+let Donkey= 44;
+let dept = 'computer Sci';
+let fac= null;
+let unseriuos= 'Shar';
+
+let door="closed", window= 'open', balconey= 'locked';
+let myAge=20;
+let yourAge= 24;
+ console.log( "i am" + myAge,  "your are"+yourAge);
+
